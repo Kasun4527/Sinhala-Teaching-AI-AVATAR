00e9cc21f300c0fbf694be04d5b91b3c7b27d59e
SachinthanaK <kumaranayakages@gmail.com>
2026-07-08 20:29:51 +0530

    fix: frontend api.js now properly uses NEXT_PUBLIC_BACKEND_URL

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://127.0.0.1:8000";
+const API = process.env.NEXT_PUBLIC_BACKEND_URL || "http://127.0.0.1:8000";
 
 export const getPreQuiz = (subject, lesson, topic) =>
   axios.get(`${API}/pre-quiz/`, {
